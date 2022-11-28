486378af2538ad51c72bef7de76ba53cf6914363
ribeiroAllison <allisonribeiro@gmail.com>
2022-11-28 14:58:27 -0300

    exercise 3

--- exerciseList2.js
+++ exerciseList2.js
@@ -58,3 +58,38 @@ console.log(isPangram("The quick brown fox jumps over the lazy dog"));
 
 
 
+// Write a function, persistence, that takes in a positive parameter num and 
+// returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+const persistence = num =>{
+    let total = num;
+    let counter = 0;
+    let toNumber = [];
+
+    do {
+        let digits = total.toString().split('');
+        toNumber = digits.map(Number);
+        total = toNumber.reduce((accumulator, currentValue) => accumulator * currentValue);
+        counter += 1;
+        
+    } while(toNumber.length !== 1);
+    return counter - 1;
+
+}
+
+console.log(persistence(39));
+
+
+
+
+
+
+
+
+
+
