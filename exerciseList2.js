42bbb8f74e12257c8850cf9516976815345341a6
ribeiroAllison <allisonribeiro@gmail.com>
2022-11-28 13:54:41 -0300

    segundo exercício

--- exerciseList2.js
+++ exerciseList2.js
@@ -29,3 +29,32 @@ const disemvowel = str =>{
 console.log(disemvowel(brStr));
 
 
+//EXERCÍCIO 2
+
+// A pangram is a sentence that contains every single letter of the alphabet at least once. 
+//For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+//because it uses the letters A-Z at least once (case is irrelevant).
+// Given a string, detect whether or not it is a pangram. 
+//Return True if it is, False if not. Ignore numbers and punctuation.
+
+const isPangram = phrase => {
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z'];
+    let lettersInPhrase = [];
+    for(letters of phrase.toLocaleLowerCase()){
+        if (!lettersInPhrase.includes(letters) && alphabet.includes(letters)){
+        lettersInPhrase.push(letters);
+        };
+    };
+    
+
+    if(lettersInPhrase.length === alphabet.length){
+    return true;
+    } else {
+    return false;
+    };
+};
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog"));
+
+
+
